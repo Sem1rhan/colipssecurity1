876dc96ef827da0a4b7d339cbbd51efa1c9d941a
Sem1rhan <150824991+Sem1rhan@users.noreply.github.com>
2023-11-27 19:21:03 +0300

    Update rol koruma sil.js

--- commands/rol koruma sil.js
+++ commands/rol koruma sil.js
@@ -17,7 +17,7 @@ const emb = new Discord.MessageEmbed()
 .setFooter(client.user.username,client.user.avatarURL())
 .setTimestamp()
 .setTitle(`***__Rol Koruma Sistemi__***`)
-.setDescription(`<a:unlem:758930479105441795> ***Silinen Rol Koruma Sistem Bilgilendirmesi***\n *Silinen rolleri yeniden oluşturur ve silenin tüm rollerini alır.*\n`)
+.setDescription(`<a:kilit:1178729393246437457> ***Silinen Rol Koruma Sistem Bilgilendirmesi***\n *Silinen rolleri yeniden oluşturur ve silenin tüm rollerini alır.*\n`)
 .addField(` **Nasıl Açacağım?**`,"`.silinen-rol-koruma aç`\n")
 .addField(` **Nasıl Kapatacağım?**`,"`.silinen-rol-koruma kapat`")
 
